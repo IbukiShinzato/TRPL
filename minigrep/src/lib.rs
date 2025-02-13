0df4388ea6596e1f7e3904bc06d24db0c9d10006
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2025-02-13 16:57:14 +0900

    finish chapter12

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,114 @@
+use std::error::Error;
+use std::fs::File;
+
+// ファイル入出力を含む入出力処理ををするのに有用なトレイトを含んでいる
+use std::env;
+use std::io::prelude::*;
+
+// tuple -> structure
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        // add validation
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        // index 0 は binary name
+        // &strだとライフタイムでの矛盾が発生するためStringのHeapで確保
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+// 戻り値はcontentsのLifetimeに依存
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // 小文字変換
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // 小文字変換して比較
+        if line.to_lowercase().contains(&query) {
+            results.push(line)
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents),
+        );
+    }
+}
---
+++ minigrep/src/main.rs
@@ -0,0 +1,29 @@
+// ライブラリクレートをバイナリクレートに持っていく
+extern crate minigrep;
+
+use std::env;
+use std::io;
+use std::process;
+
+use minigrep::Config;
+
+fn main() -> io::Result<()> {
+    // 希望の関数が2モジュール以上ネストされている場合、 関数ではなく親モジュールをスコープに導入するのが因習的です
+    let args: Vec<String> = env::args().collect();
+
+    // index 0 は binary name
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // eprintln!にすることでエラーは画面に出力
+        // 標準出力のみがファイルにリダイレクトされる。
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+
+    Ok(())
+}
